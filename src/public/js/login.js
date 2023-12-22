6700b9dca1bc39dc589c58e9ce70c42cd2ec9a34
Facurivera <facundonrivera@gmail.com>
2023-12-22 11:16:01 -0300

    Entrega final

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -6,7 +6,7 @@ const loginUser = async () => {
         const response = await fetch(`/api/sessions/login`, {
             method:"POST",
             headers: {"Content-type": "application/json; charset=UTF-8"},
-            body: JSON.stringify({email:email, pass:password})
+            body: JSON.stringify({email:email, password:password})
         });
         
         const data = await response.json();
