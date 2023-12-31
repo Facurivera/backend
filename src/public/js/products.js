7bad4f9b13de6aea62c728a4dba0e929b70cbd1f
Facurivera <facundonrivera@gmail.com>
2023-12-31 17:10:49 -0300

    Entrega final

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,24 +1,31 @@
 const obtenerIdCarrito = () => {
     return JSON.parse(localStorage.getItem("cart")) || [];
 }
-
 const agregarProductoAlCarrito = async (pid) => {
     try {
         let cart = obtenerIdCarrito();
-    
+
         const response = await fetch("/api/carts/" + cart.id + "/products/" + pid, {
             method: "POST",
-            headers: {"Content-type": "application/json; charset=UTF-8"}
+            headers: { "Content-type": "application/json; charset=UTF-8" }
         });
 
-        if (!response.ok || response.headers.get("Content-Type") !== "application/json; charset=UTF-8") {
-            console.error("Error en la respuesta del servidor");
+        if (!response.ok) {
+            console.error("Error en la respuesta del servidor:", response.status, response.statusText);
             return;
         }
 
-        const data = await response.json();
-        console.log("Se agregó al Carrito");
+        const contentType = response.headers.get("Content-Type");
+        if (contentType && contentType.includes("application/json")) {
+            const data = await response.json();
+            console.log("Se agregó al Carrito:", data);
+
+            cart = obtenerIdCarrito();
+        } else {
+            console.error("Error: La respuesta no tiene el formato JSON esperado");
+        }
     } catch (error) {
-        console.log("Error en agregar el Producto al Carrito " + error);
+        console.error("Error en agregar el Producto al Carrito: ", error);
     }
 }
+
